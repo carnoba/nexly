b834e029d531b2cd1dc6f518f713a7c5b9d45487
carnoba <carnoba9@gmail.com>
2026-06-25 00:52:38 +0500

    Remove commented lines for clarity

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
     
     print("Verifying Database Integrity...")
     subprocess.run([sys.executable, "db_setup.py"])
-    # 2. Start Dashboard
+  
     print("Launching Dashboard...")
     try:
-        # Using python -m streamlit to ensure we use the same environment
+       
         subprocess.run([sys.executable, "-m", "streamlit", "run", "nexly_ui.py"], shell=True)
     except Exception as e:
         print(f"Error: Could not launch dashboard. {e}")
